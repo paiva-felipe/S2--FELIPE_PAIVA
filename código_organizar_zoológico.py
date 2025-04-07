751450212cdc1abae0ed2ca9716ed3838f00c0eb
paiva-felipe <felipe.g.paiva7@aluno.senai.br>
2025-04-07 13:46:14 -0300

    código_zoológico
    Esta é a versão do código da main, que tem como objetivo, obter a QUANTIDADE de cada animal, informar essa quantidade ou se não tem nenhum. E no final trazer a SOMA de todos.

---
+++ código_organizar_zoológico.py
@@ -0,0 +1,26 @@
+girafas = int(input("Quantas girafas você tem no zoológico? "))
+elefantes = int(input("Quantos elefantes você tem no zoológico? "))
+macacos = int(input("Quantos macacos você tem no zoológico? "))
+zebras = int(input("Quantas zebras você tem no zoológico? "))
+cobras = int(input("Quantas cobras você tem no zoológico? "))
+print("Quantidade de animais no zoológico:")
+if girafas == 0:
+    print("Não tem girafas.")
+else:
+    print(f"Girafas: {girafas}")
+if elefantes == 0:
+    print("Não tem elefantes.")
+else:
+    print(f"Elefantes: {elefantes}")
+if macacos == 0:
+    print("Não tem macacos.")
+else:
+    print(f"Macacos: {macacos}")
+if zebras == 0:
+    print("Não tem zebras.")
+else:
+    print(f"Zebras: {zebras}")
+if cobras == 0:
+    print("Não tem cobras.")
+else:
+    print(f"Cobras: {cobras}")
